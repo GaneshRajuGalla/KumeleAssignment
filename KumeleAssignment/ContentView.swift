ed5c1130a5a207a07780409991fb39a9bd23f41c
Ganesh Raju Galla <ganeshrajugalla@gmail.com>
2026-06-02 01:17:10 +0530

    intial commit

---
+++ KumeleAssignment/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  KumeleAssignment
+//
+//  Created by Ganesh Raju Galla on 02/06/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ KumeleAssignment/KumeleAssignmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  KumeleAssignmentApp.swift
+//  KumeleAssignment
+//
+//  Created by Ganesh Raju Galla on 02/06/26.
+//
+
+import SwiftUI
+
+@main
+struct KumeleAssignmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
